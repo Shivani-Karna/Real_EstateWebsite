6d6d3b2b73436e129dcf58f130a13be6148a606e
Shivani-Karna <cwanikarna@gmail.com>
2023-06-12 18:24:07 +0545

    Deployed

---
+++ src/Component/Companies/Companies.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Companies.css'
+
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+           <div className="padding innerWidth flexCenter c-container">
+                    <img src="./prologis.png" className="src" />
+                    <img src="./tower.png" className="src" />
+                    <img src="./equinix.png" className="src" />
+                    <img src="./realty.png" className="src" />
+                    </div>         
+    </section>
+  )
+}
+
+export default Companies
---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+import {BiMenuAltRight} from 'react-icons/bi';
+
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className=" flexCenter padding innerWidth h-container">
+        <img src="./logo.png" alt="logo" width={100} />
+        <div className="flexCenter h-menu">
+          <a href="">Resendies</a>
+          <a href="">Our Value</a>
+          <a href="">Get Started</a>
+          <button className="button">
+            <a href="">Contact Us</a>
+          </button>
+          <a href=""></a>
+        </div>
+      </div>
+      <div className="menu-icon">
+        <BiMenuAltRight size={30}/>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
